afbb5a1995c4d709344bd3ece18e462331fe829c
nejdetkadir <nejdetkadir.550@gmail.com>
2021-11-17 01:57:54 +0300

    Add following type validation to user following model

--- app/models/user_following.rb
+++ app/models/user_following.rb
@@ -5,4 +5,15 @@ class UserFollowing < ApplicationRecord
 
   # validations
   validates :user, uniqueness: { scope: %i[followable_id followable_type] }
+
+  # custom callbacks
+  before_save :check_following_type
+
+  def check_following_type
+    errors.add(:user, "can follow only cast, movies or genres") if !can_be_follow.include?(self.followable_type)
+  end
+
+  def can_follow
+    ["Cast", "Movie", "Genre"]
+  end
 end
