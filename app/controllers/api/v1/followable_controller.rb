404b107a5e42529449d40a0f901be8927703e220
nejdetkadir <nejdetkadir.550@gmail.com>
2021-11-17 16:45:29 +0300

    Create followable controller

---
+++ app/controllers/api/v1/followable_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::FollowableController < ApplicationController
+  before_action :authenticate!, only: [:follow, :unfollow]
+
+  def follow(followable)
+    @followable = current_user.follow(followable)
+
+    if @followable.persisted?
+      render json: @followable, status: :created, only: [
+        :id
+      ]
+    else
+      render json: @followable.errors, status: :unprocessable_entity
+    end
+  end
+
+  def unfollow(followable)
+    current_user.unfollow(followable)
+
+    render json: {}, status: :no_content
+  end
+end
